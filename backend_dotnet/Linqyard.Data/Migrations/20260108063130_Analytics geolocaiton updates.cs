91cd6d612a9b772461d7b872ab21d9fe71cdea48
JSM33T <jasmeetsinghkainth@gmail.com>
2026-01-08 12:29:43 +0530

    added migrations

---
+++ backend_dotnet/Linqyard.Data/Migrations/20260108063130_Analytics geolocaiton updates.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Linqyard.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Analyticsgeolocaitonupdates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Analytics",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "Analytics",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Analytics");
+
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "Analytics");
+        }
+    }
+}
