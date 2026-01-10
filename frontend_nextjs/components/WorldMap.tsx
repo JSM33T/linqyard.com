06e337243fd2efd22d3f52f15d5782f74d63fac4
JSM33T <jasmeetsinghkainth@gmail.com>
2026-01-10 06:43:39 +0530

    fix lint

--- frontend_nextjs/components/WorldMap.tsx
+++ frontend_nextjs/components/WorldMap.tsx
@@ -87,12 +87,13 @@ export function WorldMap({ data, height = 400, theme }: WorldMapProps) {
   // Fix Leaflet icon issue in Next.js
   useEffect(() => {
     if (typeof window !== 'undefined') {
-      const L = require('leaflet');
-      delete (L.Icon.Default.prototype as any)._getIconUrl;
-      L.Icon.Default.mergeOptions({
-        iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
-        iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
-        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+      import('leaflet').then((L) => {
+        delete (L.Icon.Default.prototype as any)._getIconUrl;
+        L.Icon.Default.mergeOptions({
+          iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+          iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+          shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+        });
       });
     }
   }, []);
